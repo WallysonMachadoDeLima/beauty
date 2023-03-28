d598ad0e11e34fab75909d1b30458e1eba48ea6b
Rafael Felipe <rafaguiar2005@gmail.com>
2023-03-28 18:34:28 -0400

    subindo

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,15 @@ export default function Home() {
   return (
     <>
       <div className={styles.body}>
-        <div className={styles.templeat}>HOME</div>
-        <div className={styles.login}> login</div>
+        <div className={styles.templeat}>
+          <div className={styles.background} >
+          <div className={styles.div} >
+            <h1 className={styles.titulo} >Lorem ipsum <span className={styles.colorpalavra}>dolar</span></h1>
+            <h2 className={styles.subtitulo} >Entre apenas com seu numero de telefone</h2>
+          </div>
+          </div>
+        </div>
+        <div className={styles.login}> </div>
       </div>
     </>
   );
